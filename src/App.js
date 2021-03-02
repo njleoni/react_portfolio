0372881ed2499e8eb81978811a2dc99b59f9e9de
Wagon Wheel <70062069+njleoni@users.noreply.github.com>
2021-03-01 19:54:57 -0700

    final push take 3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <Router>
       <div>
-      <Routes basename="/react_portfolio" />
         <Navbar />
           <Route exact path="/" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />          
